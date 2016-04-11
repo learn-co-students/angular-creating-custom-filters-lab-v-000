bd2cb1ca961308edcebd5dde68d20391ab46bfde
Adam King <adam.king0126@gmail.com>
2016-04-11 15:35:28 -0500

    Done.

---
+++ js/app/filters/UpperFirstLetter.js
@@ -0,0 +1,10 @@
+function UpperFirstLetter() {
+  return function (str) {
+    return (str[0].toUpperCase() + str.substr(1));
+  };
+}
+
+angular
+  .module('app')
+  .filter('upperFirstLetter', UpperFirstLetter);
+
