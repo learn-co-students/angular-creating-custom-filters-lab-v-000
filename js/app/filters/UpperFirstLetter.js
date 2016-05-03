f65c0fa22d25b8a2307086e64900cded8cf63bd7
pickledyamsman <pickledyamsman@gmail.com>
2016-05-03 10:15:29 -0400

    Done.

---
+++ js/app/filters/UpperFirstLetter.js
@@ -0,0 +1,9 @@
+function UpperFirstLetter() {
+  return function(string) {
+    return string.replace(string[0], string[0].toUpperCase());
+  }
+}
+
+angular
+  .module('app')
+  .filter('upperFirstLetter', upperFirstLetter);
