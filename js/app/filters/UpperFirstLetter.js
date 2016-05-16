08f58a736d91418f8b9323ad5b4ae3d366e1dfd1
c1505 <coreym247@gmail.com>
2016-05-15 21:56:25 -0400

    Done.

---
+++ js/app/filters/UpperFirstLetter.js
@@ -0,0 +1,10 @@
+function UpperFirstLetter() {
+  return function (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+}
+
+angular
+  .module('app')
+  .filter('upperFirstLetter', UpperFirstLetter);
+
