70ac6a981cf1bdad0f8fe81622209eab1da2ab4d
Tyler Smith <oats000@hotmail.com>
2016-06-27 22:33:03 +0000

    Done.

---
+++ js/app/filters/UpperFirstLetter.js
@@ -0,0 +1,9 @@
+function upperFirstLetter() {
+  return function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+}
+
+angular
+    .module('app')
+    .filter('upperFirstLetter', upperFirstLetter);
