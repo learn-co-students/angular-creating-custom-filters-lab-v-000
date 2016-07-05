ecd62559d17823f07b2533d2fb4cb5cb03dbbb53
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-07-04 23:36:22 -0500

    Done.

---
+++ js/app/filters/UpperFirstLetter.js
@@ -0,0 +1,9 @@
+function UpperFirstLetter(){
+  return function(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+}
+
+angular
+      .module("app")
+      .filter("upperFirstLetter", UpperFirstLetter);
