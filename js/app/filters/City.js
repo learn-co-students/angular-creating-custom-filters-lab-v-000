74c37adb912ce0e29eeee708ae981799d3c9f4f3
cassaram09 <cassaram09@gmail.com>
2016-08-05 15:04:47 -0400

    Done.

---
+++ js/app/filters/City.js
@@ -0,0 +1,11 @@
+function City(){
+  return function (people, city){
+    return people.filter(function(person){
+      return person.location.city === city;
+    });
+  };
+}
+
+angular
+  .module('app')
+  .filter('city', City)
