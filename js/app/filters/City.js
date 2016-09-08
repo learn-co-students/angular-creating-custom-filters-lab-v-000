30aa5873dc954941be92d343e0a92a5afa2a25d2
agdavid <antonio.g.david@gmail.com>
2016-09-08 09:20:39 -0400

    add alternative syntax for City filter

--- js/app/filters/City.js
+++ js/app/filters/City.js
@@ -1,8 +1,19 @@
 function City() {
   return function(contacts, city) {
+    //using Array.prototype.filter syntax
     return contacts.filter(function(contact) {
-      return contact.location.city === city;
+       return contact.location.city === city;
     });
+    
+    //using vanilla JS array iterator syntax
+      // var filtered = [];
+      // for (var i = 0; i < contacts.length; i++) {
+      //   if (contacts[i].location.city === city) {
+      //     filtered.push(contacts[i]);
+      //   };
+      // }
+      // return filtered
+    
   };
 }
 
