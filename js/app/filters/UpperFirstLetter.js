280feb4120fe6b0d9e40529893787f983730d092
agdavid <antonio.g.david@gmail.com>
2016-09-08 09:27:34 -0400

    refactor UpperFirstLetter filter

--- js/app/filters/UpperFirstLetter.js
+++ js/app/filters/UpperFirstLetter.js
@@ -1,8 +1,7 @@
 function UpperFirstLetter() {
   return function(string) {
     var stringArray = string.split('')
-    var firstLetter = stringArray[0]
-    var upCaseLetter = firstLetter.toUpperCase()
+    var upCaseLetter = string.substring(0,1).toUpperCase()
     stringArray.splice(0, 1, upCaseLetter);
     var upCaseName = stringArray.join('');
     return upCaseName
