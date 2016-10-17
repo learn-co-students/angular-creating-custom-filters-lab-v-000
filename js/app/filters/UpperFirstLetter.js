e4018d34fd29d9d472a10ba065fbbb425c9e2418
wedesignstudios <deelden@gmail.com>
2016-10-17 11:13:15 -0500

    Done

---
+++ js/app/filters/UpperFirstLetter.js
@@ -0,0 +1,10 @@
+function upperFirstLetter() {
+	return function(str) {
+		var firstLetter = str[0].toUpperCase();
+		return firstLetter + str.slice(1);
+	}
+}
+
+angular
+	.module('app')
+	.filter('upperFirstLetter', upperFirstLetter)
