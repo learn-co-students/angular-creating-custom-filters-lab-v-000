2b8f102abd2ac3d3f8636e9e884bc078ddbbaeac
jbok4 <jbok4@aol.com>
2016-12-19 20:24:08 -0500

    complete, that was fun

---
+++ js/app/filters/City.js
@@ -0,0 +1,12 @@
+function City() {
+  return function (list, city) {
+
+        return list.filter(function (item) {
+            return item.location.city === city;
+        });
+    };
+}
+ 
+angular
+    .module('app')
+    .filter('city', City);
