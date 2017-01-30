6e9574dd16e5341e81166d50aefd321d59ffb4a0
Ann Lee <aplee29@users.noreply.github.com>
2017-01-29 19:07:22 -0500

    Done.

---
+++ js/app/filters/UpperFirstLetter.js
@@ -0,0 +1,9 @@
+function UpperFirstLetter() {
+  return function(word) {
+    return word[0].toUpperCase() + word.slice(1);
+  };
+}
+
+angular
+  .module('app')
+  .filter('upperFirstLetter', UpperFirstLetter);
