7ef58dfdf899c32749f20d9f18011b19e2504f7e
jasoncasares <jasonc@csdrafting.com>
2017-02-06 12:28:29 -0800

    Done.

---
+++ js/app/filters/UpperFirstLetter.js
@@ -0,0 +1,11 @@
+function UpperFirstLetter() {
+  return function (string) {
+    return string.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  };
+}
+
+angular
+  .module('app')
+  .filter('upperFirstLetter', UpperFirstLetter);
