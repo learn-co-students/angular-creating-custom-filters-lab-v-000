c9fe9fa6b705805196f097599882d096eb112c7c
mcglaser <michaelcharlesglaser@gmail.com>
2017-04-05 21:03:06 -0700

    Done.

---
+++ js/app/filters/City.js
@@ -0,0 +1,12 @@
+
+function city() {
+    return function(contacts, city) {
+        return contacts.filter(function(contact) {
+            return contact.location.city === city;
+        });
+    };
+}
+
+angular
+    .module('app')
+    .filter('city', city);
---
+++ js/app/filters/UpperFirstLetter.js
@@ -0,0 +1,10 @@
+
+function upperFirstLetter() {
+    return function(str) {
+      return str[0].toUpperCase() + str.slice(1);
+    };
+}
+
+angular
+    .module('app')
+    .filter('upperFirstLetter', upperFirstLetter);
